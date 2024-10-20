579bbffd6110f143d0218edbc10d8c4e46809e46
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-10-20 13:25:54 +0530

    add animation to menu items

--- src/components/Menus/Menus.jsx
+++ src/components/Menus/Menus.jsx
@@ -2,6 +2,8 @@ import Fruit1 from "../../assets/fruits/apple.png";
 import Fruit2 from "../../assets/fruits/orange.png";
 import Fruit3 from "../../assets/fruits/avocado.png";
 import Fruit4 from "../../assets/fruits/cherry.png";
+import { motion } from "framer-motion";
+import { FadeLeft } from "../../utility/animation";
 
 const MenusData = [
   {
@@ -10,6 +12,7 @@ const MenusData = [
     link: "/",
     price: "$3.99",
     img: Fruit1,
+    delay: 0.3,
   },
   {
     id: 2,
@@ -17,6 +20,7 @@ const MenusData = [
     link: "/",
     price: "$4.09",
     img: Fruit2,
+    delay: 0.6,
   },
   {
     id: 3,
@@ -24,6 +28,7 @@ const MenusData = [
     link: "/",
     price: "$5.00",
     img: Fruit3,
+    delay: 0.9,
   },
   {
     id: 4,
@@ -31,23 +36,44 @@ const MenusData = [
     link: "/",
     price: "$2.99",
     img: Fruit4,
+    delay: 1.2,
   },
 ];
 
 const Menus = () => {
   return (
     <section>
-      <div className="container">
-        <h1 className="text-2xl font-bold text-left pb-10">Our Menu</h1>
-        <div>
+      <div className="container pt-12 pb-20">
+        <motion.h1
+          initial={{ opacity: 0, x: -200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.3 }}
+          className="text-2xl font-bold text-left pb-10 uppercase"
+        >
+          Our Menu
+        </motion.h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {MenusData.map((menu) => (
-            <div key={menu.id}>
+            <motion.div
+              variants={FadeLeft(menu.delay)}
+              initial="hidden"
+              whileHover={{ scale: 1.1 }}
+              whileInView={"visible"}
+              className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3"
+              key={menu.id}
+            >
               <img
                 src={menu.img}
                 alt=""
-                className="w-[60px] mb-4 scale-125 transform -translate-y-6"
+                className="w-[60px] mb-4 scale-110 transform -translate-y-6"
               />
-            </div>
+              <div>
+                <h1 className="text-lg font-semibold">{menu.title}</h1>
+                <p className="text-lg font-semibold text-secondary">
+                  {menu.price}
+                </p>
+              </div>
+            </motion.div>
           ))}
         </div>
       </div>
